68ac5d4d20c9a852b0020601cbc69428ad2a3937
angelalves <angeltrashangel@gmail.com>
2024-06-21 23:45:32 -0300

    feat: init course python

---
+++ app47.py
@@ -0,0 +1,27 @@
+listt = [
+    {'name': 'Juan', 'city': 'Bogotá'},
+    {'name': 'Pedro', 'city': 'Medellín'},
+    {'name': 'Ana', 'city': 'Bogotá'},
+    {'name': 'Luis', 'city': 'Cali'},
+    {'name': 'María', 'city': 'Bogotá'},
+]
+
+
+def show_ordened_list(listtt):
+    for item in listtt:
+        print(item)
+
+
+l1 = sorted(listt, key=lambda item: item['name'])
+l2 = sorted(listt, key=lambda item: item['city'])
+
+show_ordened_list(l1)
+print()
+show_ordened_list(l2)
+
+
+def execute(func, *args):
+    return func(*args)
+
+
+print(execute(lambda x, y: x + y, 10, 20))
