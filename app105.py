908e8d4a7d822f2ba5843a2615d0bf2c0a040df1
angelalves <angeltrashangel@gmail.com>
2024-07-15 07:36:56 -0300

    course

--- app105.py
+++ app105.py
@@ -4,8 +4,13 @@ from dataclasses import dataclass
 @dataclass
 class Person:
     name: str
-    age: int
+    surname: str
+
+    def complete_name(self):
+        return f'{self.name} {self.surname}'
 
 
-p1 = Person('Luiz', 32)
+p1 = Person('Angie', 'Alves')
+
 print(p1)
+print(p1.complete_name())
